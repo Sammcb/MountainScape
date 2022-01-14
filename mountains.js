b220db7d4506d99bcd3ad9b81164cfb4b93f6040
Samuel McBroom <sam@sammcb.com>
2022-01-13 21:24:15 -0500

    Finished an initial version.

--- mountains.js
+++ mountains.js
@@ -123,11 +123,10 @@ function scaleY(y, scale, shift) {
 	return (y + 1) * scale + shift
 }
 
-function formMountain() {
+function formMountain(octaves, flatnessFactor, heightFactor, peaks, colors, gradient=false) {
 	previousSeed = seed
 
-	const wavelength = width / 2
-	const octaves = 8
+	const wavelength = width / peaks
 	const gradients = generateGradients(wavelength, octaves)
 
 	if (wavelength <= 0) {
@@ -140,17 +139,24 @@ function formMountain() {
 
 	const heights = fractal(octaves, wavelength)
 
-	const flatnessFactor = 2 // Larger is flatter
 	const scale = height / flatnessFactor
 	const heightSum = heights.reduce((first, second) => first + second) + heights.length
 	const averageHeight =  heightSum / heights.length * scale
-	const heightFactor = 0.5 // Between 0 and 1, represents where the average height of the noise will be relative to the height
 	const shift = (-height * heightFactor) + averageHeight
 
 	const padding = 5
 
 	context.beginPath()
-	context.strokeStyle = '#000000'
+	context.strokeStyle = colors[0]
+	if (gradient) {
+		const minHeight = scaleY(Math.min(...heights), scale, shift)
+		const gradient = context.createLinearGradient(0, minHeight, 0, height)
+		gradient.addColorStop(0, colors[0])
+		gradient.addColorStop(1, colors[1])
+		context.fillStyle = gradient
+	} else {
+		context.fillStyle = colors[0]
+	}
 	context.moveTo(-padding, height)
 	context.lineTo(-padding, scaleY(heights[0], scale, shift))
 
@@ -166,171 +172,56 @@ function formMountain() {
 	context.fill()
 }
 
-function draw() {
-	clearCanvas()
-	formMountain()
+function fillBackground(colors, gradient=false) {
+	if (gradient) {
+		const gradient = context.createLinearGradient(0, 0, 0, height)
+		gradient.addColorStop(0, colors[0])
+		gradient.addColorStop(1, colors[1])
+		context.fillStyle = gradient
+	} else {
+		context.fillStyle = colors[0]
+	}
+	context.fillRect(0, 0, width, height)
 }
 
+function draw() {
+	clearCanvas()
 
-// function mountainY = (x, params, shift=0) => {
-// 		let y = 0
-// 		for (param of params) {
-// 			y += 
-// 		}
-// 		params.forEach(param => y += param.a * (param.sin ? Math.sin(x * param.b + param.c) : Math.cos(x * param.b + param.c)))
-// 		return height * y + shift
-// 	};
+	fillBackground(['#cbe8ff'])
 
-// function placeMountain(context) {
-// 	// Background mountains
-// 	// Range for a = [0.01, 0.15]
-// 	// Range for b = [0.005, 0.03]
-// 	// Range for c = [-100, 100]
-// 	let bgParams = []
-// 	// Range for number of functions = [4, 10]
-// 	let bgMax = rRange(4, 10)
-// 	for (let i = 1; i < bgMax; i++) bgParams.push({'sin': rRange(0, 1) >= 0.5, a: rRange(0.01, 0.15), b: rRange(0.005, 0.03), c: rRange(-100, 100)})
-// 	grd = context.createLinearGradient(0, 0, 0, canvas.height)
-// 	grd.addColorStop(0, 'tan')
-// 	// Range for dark brown to light brown transition = [0.3, 0.5]
-// 	grd.addColorStop(rRange(0.3, 0.5), 'sienna')
-// 	context.fillStyle = grd
-// 	context.beginPath()
-// 	context.moveTo(0, height)
-// 	// Range for step size = [20, 30]
-// 	// for (let x = 0; x <= width + 30; x += rRange(10, 20)) context.lineTo(x, mtn(x, bgParams, height / 2 - 50))
-// 	// for (let x = 0; x < width; x += 1) {
-// 	// 	context.lineTo(x, mtn(x, bgParams, height / 2 - 50))
-// 	// }
+	// Larger = rougher terrain
+	let octaves = 8
+	// Larger = flatter mountains
+	let flatnessFactor = 3
+	// Represents where the average height of the noise will be relative to the height
+	let heightFactor = 0.3
+	// The number of mountain peaks
+	let peaks = 5
+	// Color of the mountain
+	let colors = ['#a0d5ff', '#498ec4']
+	formMountain(octaves, flatnessFactor, heightFactor, peaks, colors, gradient=true)
 
-// 	// Simulate water erosion
-// 	let maxDrops = 1
-// 	let flatThreshold = 5
-// 	let flowDirection = (atX, atY) => {
-// 		if (atX > 0) {
-// 			let leftY = mtn(atX - 1, bgParams, height / 2 - 50)
-// 			if (leftY - atY)
-// 		}
-// 		if (atX < width) {
+	flatnessFactor = 3
+	heightFactor = 0.05
+	peaks = 3
+	colors = ['#498ec4', '#1d6caa']
+	formMountain(octaves, flatnessFactor, heightFactor, peaks, colors, gradient=true)
 
-// 		}
-// 		return 0
-// 	}
-// 	for (let drop = 0; drop < maxDrops; drop++) {
-// 		let dropX = rRange(0, width)
-// 		let dropY = mtn(dropX, bgParams, height / 2 - 50)
+	flatnessFactor = 4
+	heightFactor = -0.2
+	peaks = 3
+	colors = ['#1d6caa', '#0e5a95']
+	formMountain(octaves, flatnessFactor, heightFactor, peaks, colors, gradient=true)
 
-// 	}
-// 	context.lineTo(width, height)
-// 	context.closePath()
-// 	context.fill()
-// }
+	flatnessFactor = 2
+	heightFactor = 0.25
+	peaks = 3
+	colors = ['#0e5a95', '#153b59']
+	formMountain(octaves, flatnessFactor, heightFactor, peaks, colors, gradient=true)
 
-// function mtnGen() {
-// 	// Rng
-// 	let lcg = x => seed = (1103515245 * x + 12345) % Math.pow(2, 31)
-// 	let rMax = Math.pow(2, 31) - 1
-// 	let rRange = (min, max) => lcg(seed) / rMax * (max - min) + min
-
-// 	// Canvas setup
-// 	canvas = document.getElementById('mtn')
-// 	canvas.style.height = `${Math.floor(canvas.offsetWidth / 2)}px`
-// 	let context = canvas.getContext('2d')
-// 	let width = canvas.offsetWidth
-// 	let height = canvas.offsetWidth / 2
-// 	let scale = window.devicePixelRatio
-// 	canvas.width = width * scale
-// 	canvas.height = height * scale
-// 	context.scale(scale, scale)
-// 	context.clearRect(0, 0, canvas.width, canvas.height)
-// 	context.strokeStyle = 'rgba(0, 0, 0, 0)'
-
-// 	let mtn = (x, params, shift=0) => {
-// 		let y = 0
-// 		params.forEach(param => y += param.a * (param.sin ? Math.sin(x * param.b + param.c) : Math.cos(x * param.b + param.c)))
-// 		return height * y + shift
-// 	};
-
-// 	// Background
-// 	// bg color gradient fill (pink to purple, purple to blue, etc)
-// 	let background = context.createLinearGradient(0, 0, 0, canvas.height)
-// 	background.addColorStop(0, 'rebeccapurple')
-// 	// Range for pink to purple transition = [0.2, 0.4]
-// 	background.addColorStop(rRange(0.2, 0.4), 'lightpink')
-// 	context.fillStyle = background
-// 	context.fillRect(0, 0, canvas.width, canvas.height)
-
-// 	// Sun/Moon
-// 	// Range for y = [100, 400]
-// 	let skyY = rRange(100, 400)
-// 	let sun = context.createRadialGradient(800, skyY, 10, 800, skyY, 60)
-// 	sun.addColorStop(0, 'gold')
-// 	sun.addColorStop(1, 'rgba(255, 215, 0, 0.1')
-// 	context.fillStyle = sun
-// 	context.beginPath()
-// 	context.arc(800, skyY, 50, 0, 2 * Math.PI)
-// 	context.fill()
-
-// 	placeMountain(context)
-	
-// 	// // Midground mountains
-// 	// // Range for a = [0.01, 0.1]
-// 	// // Range for b = [0.01, 0.03]
-// 	// // Range for c = [-100, 100]
-// 	// let mgParams = []
-// 	// // Range for number of functions = [4, 8]
-// 	// let mgMax = rRange(4, 8)
-// 	// for (let i = 0; i < mgMax; i++) mgParams.push({'sin': rRange(0, 1) >= 0.5, a: rRange(0.01, 0.1), b: rRange(0.01, 0.03), c: rRange(-100, 100)})
-// 	// grd = ctx.createLinearGradient(0, 0, 0, c.height)
-// 	// grd.addColorStop(0.3, 'darkolivegreen')
-// 	// // Range for dark green to light green transition = [0.6, 0.8]
-// 	// grd.addColorStop(rRange(0.4, 0.5), 'darkgreen')
-// 	// ctx.fillStyle = grd
-// 	// ctx.beginPath()
-// 	// ctx.moveTo(0, h)
-// 	// // Range for step size = [20, 30]
-// 	// for (let x = 0; x <= w + 30; x += rRange(20, 30)) ctx.lineTo(x, mtn(x, mgParams, h / 2 + 20))
-// 	// ctx.lineTo(w, h)
-// 	// ctx.closePath()
-// 	// ctx.fill()
-
-// 	// // Foreground mountains
-// 	// // Range for a = [0.01, 0.07]
-// 	// // Range for b = [0.01, 0.03]
-// 	// // Range for c = [-100, 100]
-// 	// let fgParams = []
-// 	// // Range for number of functions = [4, 10]
-// 	// let fgMax = rRange(4, 10)
-// 	// for (let i = 0; i < fgMax; i++) fgParams.push({'sin': rRange(0, 1) >= 0.5, a: rRange(0.01, 0.07), b: rRange(0.01, 0.03), c: rRange(-100, 100)})
-// 	// grd = ctx.createLinearGradient(0, 0, 0, c.height)
-// 	// grd.addColorStop(0, 'lightgreen')
-// 	// // Range for dark green to light green transition = [0.7, 0.9]
-// 	// grd.addColorStop(rRange(0.7, 0.9), 'forestgreen')
-// 	// ctx.fillStyle = grd
-// 	// ctx.beginPath()
-// 	// ctx.moveTo(0, h)
-// 	// // Range for step size = [20, 30]
-// 	// for (let x = 0; x <= w + 30; x += rRange(20, 30)) ctx.lineTo(x, mtn(x, fgParams, h / 2 + 100))
-// 	// ctx.lineTo(w, h)
-// 	// ctx.closePath()
-// 	// ctx.fill()
-
-// 	// // Lake
-// 	// // Range for a = [0.06, 0.12]
-// 	// // Range for b = [0.004, 0.0045]
-// 	// // Range for c = [675, 725]
-// 	// let lParams = [
-// 	// 	{'sin': true, a: rRange(0.06, 0.12), b: rRange(0.004, 0.0045), c: rRange(675, 725)}
-// 	// ]
-// 	// grd = ctx.createLinearGradient(0, 0, 0, c.height)
-// 	// // Range for dark blue to light blue transition = [0.75, 0.85]
-// 	// grd.addColorStop(rRange(0.75, 0.85), 'dodgerblue')
-// 	// grd.addColorStop(1, 'mediumblue')
-// 	// ctx.fillStyle = grd
-// 	// ctx.beginPath()
-// 	// ctx.moveTo(0, h)
-// 	// for (let x = 0; x <= w; x++) ctx.lineTo(x, mtn(x, lParams, h / 2 + 250))
-// 	// ctx.lineTo(w, h)
-// 	// ctx.closePath()
-// 	// ctx.fill()
-// }
+	flatnessFactor = 4
+	heightFactor = -0.4
+	peaks = 2
+	colors = ['#153b59', '#001f37']
+	formMountain(octaves, flatnessFactor, heightFactor, peaks, colors, gradient=true)
+}
